34023f89c235df1ec925ddb1fb632882bb1991ac
svatowl-tech <svat.owl@gmail.com>
2026-02-18 01:07:54 +0300

    feat: Update dependencies and Tailwind config
    Updates React and Vite dependencies to their latest versions. Refactors Tailwind CSS content paths to explicitly include main application files and component directories, improving build efficiency and clarity.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
